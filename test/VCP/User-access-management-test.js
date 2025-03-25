f04364d68c78de22415fb2b24bebac9b5beaf0a1
cristinamartinez1394 <crismartinez1394@gmail.com>
2025-03-25 14:53:15 -0500

    Ajuste de localizadores para VCP/User access management

--- test/VCP/User-access-management-test.js
+++ test/VCP/User-access-management-test.js
@@ -27,8 +27,8 @@ module.exports={
         .waitForElementVisible('h1.flex',30000)
         .assert.textContains('h1.flex', 'welcome to the Cloud Control Platform')
         .click('button[type="button"] span')
-        .click('li:nth-child(11) button:nth-child(1)')
-        .click('div:nth-child(2) > h2:nth-child(1)')
+        .click('li:nth-child(10) button:nth-child(1)')
+        .click('h2.text-sm.font-normal.false')
         .assert.textContains('p.items-center', 'User Access Management')
         },
 
@@ -40,12 +40,12 @@ module.exports={
             .assert.textContains('td.whitespace-normal', 'Andres')
 
             //Operator
-            .click(':nth-child(2) > span:nth-child(1)')
+            .click('ul > li:nth-child(2) > button')
             .setValue('input.w-full','Jonas')
             .assert.textContains('td.whitespace-normal', 'Jonas')
 
             //Administrator
-            .click('li:nth-child(3) > span:nth-child(1)')
+            .click('ul:nth-child(1) > li:nth-child(3) > button:nth-child(1)')
             .setValue('input.w-full','Aura')
             .assert.textContains('td.whitespace-normal', 'Aura')
 
@@ -55,17 +55,17 @@ module.exports={
         'Scenario 3: Search for an invalid element':function(client){
             client
             //User
-            .click('span.cursor-pointer')
+            .click('ul:nth-child(1) > li:nth-child(1) > button:nth-child(1)')
             .setValue('input.w-full','Testing123')
             .assert.textContains('p.text-red', 'No items found')
 
             //Operator
-            .click(':nth-child(2) > span:nth-child(1)')
+            .click('ul > li:nth-child(2) > button')
             .setValue('input.w-full','Testing123')
             .assert.textContains('p.text-red', 'No items found')
 
             //Administrator
-            .click('li:nth-child(3) > span:nth-child(1)')
+            .click('ul:nth-child(1) > li:nth-child(3) > button:nth-child(1)')
             .setValue('input.w-full','Testing123')
             .assert.textContains('p.text-red', 'No items found')
         }
